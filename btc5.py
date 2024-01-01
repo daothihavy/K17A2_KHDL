22d9544febb6ea9f8f0fc716785c5ebce92250f7
Khoa hoc du lieu 17A2 <dthvy.dhkl17a2hn@sv.uneti.edu.vn>
2024-01-01 23:48:53 +0700

    Thêm fille

---
+++ btc5.py
@@ -0,0 +1,27 @@
+# bài 1
+print("**  **  ******  **      **       ******")
+print("**  **  **      **      **       **  **")
+print("******  ******  **      **       **  **")
+print("**  **  **      **      **       **  **")
+print("**  **  ******  ******  *******  ******")
+
+
+# bài 2
+x = 10
+y = 5
+print(f"x = {x}, y = {y}")
+print(f"Tong x+y = {x+y}")
+print(f"Hieu x-y = {x-y}")
+print(f"Tich x*y = {x*y}")
+print(f"Thuong x/y = {x/y}")
+
+
+# bài 3
+ten_hang = "Sữa hộp Vina Milk"
+so_luong = 5
+don_gia = 25000
+tien_phai_tra = so_luong*don_gia
+print(f"Ten hang: {ten_hang}")
+print(f"So luong: {so_luong}")
+print(f"Don gia: {don_gia}")
+print(f"Tien phai tra: {tien_phai_tra} vnd")
